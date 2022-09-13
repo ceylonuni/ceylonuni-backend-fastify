3307c63047e63071f0592e59a5ba69676f2133cb
sachchin06 <sachchin06@gmail.com>
2022-09-13 09:23:57 +0530

    add , update apis added for university , course

--- routes/api/course/index.js
+++ routes/api/course/index.js
@@ -1,4 +1,5 @@
 "use strict";
+var moment = require('moment');
 module.exports = async function (fastify, opts) {
     fastify.get(
         "/all",
@@ -24,5 +25,95 @@ module.exports = async function (fastify, opts) {
           }
         }
         );
-    };
+    
+    fastify.post(
+          "/add",
+          {
+            schema: {
+              tags: ["Admin"],
+              body: {
+                type: "object",
+                required: ["name"],
+                properties: {
+                  name: {
+                    type: "string",
+                    default: "Name",
+                  },
+                },
+              },
+            },
+          },
+          async (request, reply) => {
+            try {
+      
+              // var item = await fastify.prisma.courses.findUnique({
+              //   where: {
+              //     name: request.body.name,
+              //   },
+              // });
+              // if (item) {
+              //   throw new Error("The course is alreday registered.");
+              // } else {
+                
+                var item = await fastify.prisma.courses.create({
+                  data: {
+                    name: request.body.name,
+                    created_at:moment().toISOString(),
+                    updated_at:moment().toISOString(),
+                  },
+                });
+
+                ;
+                reply.send(item);
+              // }
+              
+            } catch (error) {
+              reply.send(error);
+            } finally {
+              await fastify.prisma.$disconnect();
+            }
+          }
+     );
+
+     fastify.post(
+      "/update",
+      {
+        schema: {
+          tags: ["Admin"],
+          body: {
+            type: "object",
+            required: ["id", "name"],
+            properties: {
+              id: {
+                type: "integer",
+                default: 1,
+              },
+              name: {
+                type: "string",
+                default: "name",
+              },
+            },
+          },
+        },
+      },
+      async (request, reply) => {
+        try {
+          var item = await fastify.prisma.courses.update({
+            where: {
+              id: request.body.id,
+            },
+            data: {
+              name: request.body.name,
+              updated_at:moment().toISOString(),
+            },
+          });
+          reply.send(item);
+        } catch (error) {
+          reply.send(error);
+        } finally {
+          await fastify.prisma.$disconnect();
+        }
+      }
+    );
+};
     
