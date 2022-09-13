b03ebdc3eb9f2f73eefef1ae8cb6c8f98cba7ea3
sachchin06 <sachchin06@gmail.com>
2022-09-13 12:35:24 +0530

    login api added

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -72,19 +72,78 @@ module.exports = async function (fastify, opts) {
             updated_at: moment().toISOString(),
           },
         });
-        const salt = await bcrypt.genSalt(10);
-        const password = await bcrypt.hash(request.body.password, salt);
+        // const salt = await bcrypt.genSalt(10);
+        // const password = await bcrypt.hash(request.body.password, salt);
+
+        const password = await bcrypt.hash(request.body.password, 10);
         var account = await fastify.prisma.accounts.create({
           data: {
             student_id: student.id,
-            username:request.body.first_name,
+            username: request.body.first_name,
             email: request.user.email,
             password: password,
             created_at: moment().toISOString(),
             updated_at: moment().toISOString(),
           },
         });
-        reply.send({message:'success'});
+        reply.send({ message: "success" });
+      } catch (error) {
+        reply.send(error);
+      } finally {
+        await fastify.prisma.$disconnect();
+      }
+    }
+  );
+
+  fastify.post(
+    "/login",
+    {
+     
+      schema: {
+        tags: ["Auth"],
+        body: {
+          type: "object",
+          required: ["email", "password"],
+          properties: {
+            email: {
+              type: "string",
+              default: "Last name",
+            },
+            password: {
+              type: "string",
+              default: "Password",
+            },
+          },
+        },
+      },
+    },
+    async (request, reply) => {
+      try {
+        var item = await fastify.prisma.accounts.findUnique({
+          where: {
+            email: request.body.email,
+          },
+        });
+        if (item) {
+          console.log(item);
+          let pass = request.body.password;
+          let hash = item.password;
+
+          const result = await bcrypt.compare(pass, hash);
+
+          if (result) {
+            const token = fastify.jwt.sign({
+              email: item.email,
+              id: item.id,
+              student_id: item.student_id,
+            });
+            reply.send(token);
+          } else {
+            reply.send("not ok");
+          }
+        } else {
+          throw new Error("This email don't have an account");
+        }
       } catch (error) {
         reply.send(error);
       } finally {
