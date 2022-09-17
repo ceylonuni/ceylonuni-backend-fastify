59def1247b4c8e785319c693345ee999e9dbc5f0
sachchin06 <sachchin06@gmail.com>
2022-09-17 20:21:19 +0530

    profile-get, profile-update, profile-delete apis added

---
+++ routes/api/profile/index.js
@@ -0,0 +1,134 @@
+"use strict";
+const moment = require("moment");
+module.exports = async function (fastify, opts) {
+  fastify.get(
+    "/get",
+    {
+      preValidation: [fastify.verifyEmail],
+      schema: {
+        security: [{ bearerAuth: [] }],
+        tags: ["Auth"],
+      },
+    },
+    async (request, reply) => {
+      try {
+        var item = await fastify.prisma.students.findMany({
+          where: {
+            deleted_at: null,
+            id: request.user.student_id,
+          },
+          select: {
+            first_name: true,
+            last_name: true,
+            mobile: true,
+            address: true,
+          },
+        });
+
+        reply.send(item);
+      } catch (error) {
+        reply.send(error);
+      } finally {
+        await fastify.prisma.$disconnect();
+      }
+    }
+  );
+
+  fastify.post(
+    "/update",
+    {
+      preValidation: [fastify.authenticate],
+      schema: {
+        tags: ["Auth"],
+        security: [{ bearerAuth: [] }],
+        body: {
+          type: "object",
+          properties: {
+            first_name: {
+              type: "string",
+              default: "name",
+            },
+            last_name:  {
+              type: "string",
+              default: "name",
+            },
+            mobile: {
+              type: "string",
+              default: "mobile",
+            },
+            address: {
+                type: "string",
+                default: "address",
+              },
+          },
+        },
+      },
+    },
+    async (request, reply) => {
+
+      try {
+        var item = await fastify.prisma.students.update({
+            where: {
+              id: request.user.id,
+            },
+            data: {
+              first_name: request.body.first_name,
+              last_name: request.body.last_name,
+              mobile: request.body.mobile,
+              address: request.body.address,
+              updated_at:moment().toISOString(),
+            },
+          });
+          
+        reply.send(item);
+
+      } catch (error) {
+        reply.send(error);
+      } finally {
+        await fastify.prisma.$disconnect();
+      }
+    }
+  );
+  
+  fastify.post(
+    "/delete",
+    {
+      preValidation: [fastify.authenticate],
+      schema: {
+        tags: ["Auth"],
+        security: [{ bearerAuth: [] }],
+        body: {
+          type: "object",
+          properties: {
+            email: {
+              type: "string",
+              default: "email",
+            },
+          },
+        },
+      },
+    },
+    async (request, reply) => {
+
+      try {
+        var item = await fastify.prisma.accounts.update({
+            where: {
+            //   id: request.user.id,
+            email: request.body.email,
+            },
+            data: {
+              deleted_at: moment().toISOString(),
+            },
+          });
+          
+        reply.send('Account Deleted');
+
+      } catch (error) {
+        reply.send(error);
+      } finally {
+        await fastify.prisma.$disconnect();
+      }
+    }
+  );
+
+};
