a4d16acffe49ef0383aa85afe926f6da042fff64
vniroshan <nnirosh447@gmail.com>
2023-02-02 12:53:48 +0530

    event added

--- services/event/routes/api/event/v1/index.js
+++ services/event/routes/api/event/v1/index.js
@@ -4,6 +4,95 @@ const moment = require("moment");
 
 module.exports = async function (fastify, opts) {
   fastify.post(
+    "/read",
+    {
+      preValidation: [fastify.authenticate],
+      schema: {
+        security: [{ bearerAuth: [] }],
+        tags: ["Auth"],
+      },
+      body: {
+        type: "object",
+        properties: {
+          key: {
+            type: "string",
+            default: "egdnssjc-jjahdnd-nnakakhd",
+          },
+        },
+      },
+    },
+    async (request, reply) => {
+      try {
+        var item = await fastify.prisma.events.findUnique({
+          where: {
+            key: request.body.key,
+          },
+          select: {
+            id:true,
+            name:true,
+            key:true,
+            image_url: true,
+            start_at: true,
+            end_at : true,
+            venue : true,
+            student_id:true,
+            posts:{
+              select: {
+                id: true,
+                key: true,
+                text: true,
+                image_url: true,
+                video_url: true,
+                created_at: true,
+                students: {
+                  select: {
+                    first_name: true,
+                    last_name:true,
+                    image_url: true,
+                  },
+                },
+                comments:{
+                  select:{
+                    text:true,
+                    created_at:true,
+                    students: {
+                      select: {
+                        first_name: true,
+                        last_name:true,
+                        image_url: true,
+                      },
+                    },
+                  }
+                },
+                likes:{
+                  select:{
+                    students: {
+                      select: {
+                        id:true,
+                        first_name: true,
+                        last_name:true,
+                        image_url: true,
+                      },
+                    },
+                  }
+                }
+              },
+              orderBy: {
+                id: "desc",
+              },
+            }
+          },
+        });
+
+        reply.send(item);
+      } catch (error) {
+        reply.send(error);
+      } finally {
+        await fastify.prisma.$disconnect();
+      }
+    }
+  );
+  fastify.post(
     "/add",
     {
       preValidation: [fastify.authenticate],
@@ -17,7 +106,9 @@ module.exports = async function (fastify, opts) {
             name: {
               type: "string",
             },
-
+            image: {
+              type: "string",
+            },
             start_at: {
               type: "string",
             },
@@ -34,20 +125,39 @@ module.exports = async function (fastify, opts) {
     async (request, reply) => {
       try {
         var key = uuidv4();
-
         var event = await fastify.prisma.events.create({
           data: {
             name: request.body.name,
             key: key,
             student_id: request.user.student_id,
             venue: request.body.venue,
-            start_at: moment().toISOString(),
-            end_at: moment().toISOString(),
+            start_at: request.body.start_at,
+            end_at: request.body.end_at,
             created_at: moment().toISOString(),
             updated_at: moment().toISOString(),
           },
         });
-        reply.send(event);
+
+        var image_url = null
+        if(request.body.image){
+          image_url = await fastify.image.upload({
+            image_url: request.body.image,
+            key: key,
+          })  
+
+          var item = await fastify.prisma.events.update({
+            where: {
+              id: event.id,
+            },
+            data: {
+              image_url: image_url,
+              updated_at:moment().toISOString(),
+            },
+          });
+        }
+       
+        
+        reply.send(item);
       } catch (error) {
         reply.send(error);
       } finally {
