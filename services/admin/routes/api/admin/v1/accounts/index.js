84ef62108fba669b20c851f57c76e6d699c47c5f
sachchin06 <sachchin06@gmail.com>
2023-03-23 14:17:34 +0530

    account, comment apis added

--- services/admin/routes/api/admin/v1/accounts/index.js
+++ services/admin/routes/api/admin/v1/accounts/index.js
@@ -1,27 +1,80 @@
 "use strict";
+var moment = require("moment");
+
 module.exports = async function (fastify, opts) {
-    fastify.get(
-        "/all",
-        {
-          schema: {
-            tags: ["Admin"],
+  fastify.get(
+    "/all",
+    {
+      schema: {
+        tags: ["Admin"],
+      },
+    },
+    async (request, reply) => {
+      try {
+        var items = await fastify.prisma.accounts.findMany({
+          where: {
+            deleted_at: null,
+          },
+        });
+
+        reply.send(items);
+      } catch (error) {
+        reply.send(error);
+      } finally {
+        await fastify.prisma.$disconnect();
+      }
+    }
+  );
+
+  fastify.post(
+    "/delete",
+    {
+      schema: {
+        tags: ["Admin"],
+        body: {
+          type: "object",
+          required: ["ac_id"],
+          properties: {
+            ac_id: {
+              type: "integer",
+              default: 2,
+            },
           },
         },
-        async (request, reply) => {
-          try {
-            var items = await fastify.prisma.accounts.findMany({
-                where:{
-                    deleted_at:null
-                }
-            });
-           
-            reply.send(items);
-          } catch (error) {
-            reply.send(error);
-          } finally {
-            await fastify.prisma.$disconnect();
-          }
-        }
-        );
-    };
-    
+      },
+    },
+    async (request, reply) => {
+      try {
+        const account = await fastify.prisma.accounts.findUnique({
+          where: {
+            id: request.body.ac_id,
+          },
+        });
+
+        var updatedStudent = await fastify.prisma.students.update({
+          where: {
+            id: account.student_id,
+          },
+          data: {
+            deleted_at: moment().toISOString(),
+          },
+        });
+
+        var updatedAccount = await fastify.prisma.accounts.update({
+          where: {
+            id: request.body.ac_id,
+          },
+          data: {
+            deleted_at: moment().toISOString(),
+          },
+        });
+
+        reply.send("Account Deeleted");
+      } catch (error) {
+        reply.send(error);
+      } finally {
+        await fastify.prisma.$disconnect();
+      }
+    }
+  );
+};
